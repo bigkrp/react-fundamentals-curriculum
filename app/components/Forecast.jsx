6437b398d85b35fee55a4989368575cf8f9b9f0b
krp-arina <krp-arina@localhost.localdomain>
2016-05-25 20:56:24 +0300

    Make forecast page

---
+++ app/components/Forecast.jsx
@@ -0,0 +1,42 @@
+import React, {PropTypes} from 'react';
+
+const DayItem = (props) => 
+  <div className='forecast-day'>
+    <img src={'./images/weather-icons/' + props.day.weather[0].icon + '.svg'}
+    alt={props.day.weather[0].description}/>
+    <h2>{props.day.key}</h2>
+  </div>;
+
+DayItem.propTypes = {
+  day: PropTypes.object.isRequired
+};
+
+const ForecastUi = (props)=>
+  <div className="forecast-container">
+    <h1>{props.city}</h1>
+    <div className="forecast-day__container">
+      {props.daysData.map((item)=>
+        <DayItem key={item.dt} day={item} />
+      )}
+    </div>
+  </div>;
+
+ForecastUi.propTypes = {
+  city: PropTypes.string,
+  daysData: PropTypes.array
+};
+
+export const Forecast = (props) => {
+  console.log(props.daysData);
+  return props.isLoading
+    ? <h1>Loading</h1>
+    : <ForecastUi
+        city={props.city}
+        daysData={props.daysData}/>;
+};
+
+Forecast.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  city: PropTypes.string,
+  daysData: PropTypes.array
+};
--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -1,34 +1,31 @@
 import React, {PropTypes} from 'react';
 import {getFiveDaysForecast} from '../helpers/weatherAPI';
+import {Forecast} from '../components/Forecast';
 
 export default class ForecastContainer extends React.Component {
   constructor(props) {
     super(props);
   }
   state = {
-    daysData: null
+    daysData: null,
+    isLoading: true
   }
   componentWillMount(){
     getFiveDaysForecast(this.props.routeParams.city)
       .then((resp)=>{
-        this.setState({daysData: resp.data.list});
+        this.setState({daysData: resp.data.list, isLoading: false});
         console.log(this.state);
       });
   }
   render() {
     return (
-      <div>
-        {this.state.daysData.forEach((val, day)=>{
-          <div>
-            <div>{val.weather.icon}</div>
-            <h2>Day {day}</h2>
-            <h3>{val.weather.description}</h3>
-          </div>
-        })}
-      </div>
+      <Forecast
+        city={this.props.routeParams.city}
+        daysData={this.state.daysData}
+        isLoading={this.state.isLoading} />
     );
   }
-}
+};
 
 ForecastContainer.propTypes = {
 };
